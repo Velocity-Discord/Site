30d9bffa79892ef5521d273bf8326dfc709a636a
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-03-21 09:29:48 +1100

    fix CMS

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/css')
     eleventyConfig.addPassthroughCopy("./src/js");
     eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addPassthroughCopy("./src/admin")
 
     return {
         dir: {
