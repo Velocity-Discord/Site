5ceabc42341928de9837429f96dd2e57eebb9938
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-09-13 16:27:29 +1000

    bye bye ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
-    output: "server",
     vite: {
         ssr: {
             noExternal: ["@astrojs/prism"],
         },
     },
-    adapter: netlify(),
 });
