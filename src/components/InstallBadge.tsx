e6479a206ad20aa39df97b937cc983e08132aad7
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-10-05 08:28:54 +1100

    ughughughugh

--- src/components/InstallBadge.tsx
+++ src/components/InstallBadge.tsx
@@ -1,8 +1,21 @@
-import "../styles/InstallBadge.css";
-
-export default (props: any) => {
+export default () => {
     return (
         <button
+            style={{
+                backgroundColor: "#1a191c",
+                border: "0",
+                borderRadius: "0",
+                color: "#fff",
+                fontSize: "14px",
+                fontWeight: "500",
+                cursor: "pointer",
+                width: "120px",
+                height: "35px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                textDecoration: "none",
+            }}
             className="install-badge"
             onClick={async () => {
                 if (!window.ws) window.ws = new WebSocket("ws:localhost:1842");
